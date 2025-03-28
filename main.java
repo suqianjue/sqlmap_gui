c7abf03109b0d6a7fb567af96294c4dc86654d05
suqianjue <81636330+suqianjue@users.noreply.github.com>
2025-03-28 11:28:39 +0800

    Add files via upload

---
+++ main.java
@@ -0,0 +1,8 @@
+package org.example.toolsui;
+
+public class main {
+    public static void main(String[] args) {
+        System.setProperty("file.encoding", "UTF-8");
+        PythonScriptRunner.main(args);
+    }
+}
